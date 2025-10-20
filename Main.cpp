33f6fa539053f79f6045e0d946b21cf4082e1a67
Michael Franks <mffranks34@gmail.com>
2025-10-20 22:13:10 +0100

    final day commit

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
-#include <hash_map>
+#include <vector>
 #include "Cell.h"
 #include "Point.h"
 
 int main()
 {
-    // creating a space for a game board 
-    
+    // creating a space for a game board
+
     Cell cell;
 
-    Point point(2,1);
-    Point point2(2,1);
+    Point point(0,0);
+    Point point2(2, 1);
 
-    if (point == point2)
+    std::vector<Point> data = point.neighbours();
+
+    for (int i = 0; i < data.size(); i++)
     {
-        //code code
-        std::cout << "Points match \n";
-    }
-    else
-    {
-        std::cout << "Points don't match \n";
+        std::cout << i <<". in hash code " << data[i].hash_code() << "\n";
     }
 
-    std::cout << "hello world \n";
+
     return 0;
 }
