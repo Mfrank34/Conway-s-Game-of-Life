8f29bc5050f15b1831f69a3ff985b9d6eddc2846
Michael Franks <mffranks34@gmail.com>
2025-10-20 17:49:14 +0100

    changes to tasks file for vscode to see headers and source

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include "header/Cell.h"
+//#include "header/Cell.h"
 
 int main()
 {
-    Cell cell;
     std::cout << "hello world \n";
-    
-    
-    
-    
     return 0;
 }
